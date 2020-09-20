73e57ffed27b254d11aa6b12819d75bf82493962
ryosuke takasawa <ten.r0619@gmail.com>
2020-09-21 07:07:57 +0900

    add cancell

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
+  has_many :tickets, dependent: :destroy
   belongs_to :owner, class_name: "User"
-  has_many :tickets
 
   validates :name, length: {maximum:50}, presence: true
   validates :place, length: {maximum:100}, presence: true
