22d3ed58f12504b62ef66d3972d6debc22cc9b31
ryosuke takasawa <ten.r0619@gmail.com>
2020-09-30 06:50:44 +0900

    update ominiauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development? || Rails.env.test?
     provider :github,
-      Rails.application.credentials.github[:client_id],
-      Rails.application.credentials.github[:client_secret]
+      client_id: ENV["CLIENT_ID"],
+      client_secret: ENV["CLIENT_SECRET"]
   else
     provider :github,
       client_id: ENV["CLIENT_ID"],
